f04748155fe24df78db8cae185d0a9a12c549817
Drew <bs.of.dth@gmail.com>
2013-09-29 14:20:16 -0500

    Add check to ensure battery is discharging before hibernating.

--- scripts/auto-hibernate.sh
+++ scripts/auto-hibernate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+stat=$(cat /sys/class/power_supply/BAT0/uevent | grep STATUS | cut -f2 -d=)
 batperc=$(cat /sys/class/power_supply/BAT0/uevent | grep CAPACITY | cut -f2 -d=)
-if [[ $batperc -eq "2" ]]; then
+if [[ $stat -eq "Discharging" ]] && [[ $batperc -eq "2" ]]; then
   /usr/bin/systemctl hibernate
 fi
