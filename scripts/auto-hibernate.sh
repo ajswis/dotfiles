e4bd9d287f07d0ccb1ca0be24273cde10b4f8b6a
Andrew Swistak <bs.of.dth@gmail.com>
2014-02-28 03:01:30 -0600

    Change low power hibernate to 5%

--- scripts/auto-hibernate.sh
+++ scripts/auto-hibernate.sh
@@ -2,6 +2,6 @@
 
 stat=$(cat /sys/class/power_supply/BAT0/uevent | grep STATUS | cut -f2 -d=)
 batperc=$(cat /sys/class/power_supply/BAT0/uevent | grep CAPACITY | cut -f2 -d=)
-if [[ $stat -eq "Discharging" ]] && [[ $batperc -eq "2" ]]; then
+if [[ $stat -eq "Discharging" ]] && [[ $batperc -eq "5" ]]; then
   /usr/bin/systemctl hibernate
 fi
