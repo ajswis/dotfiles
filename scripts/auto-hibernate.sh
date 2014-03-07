36413373d48525e94bd758221150a5fd5bfa2109
Andrew Swistak <bs.of.dth@gmail.com>
2014-03-07 06:24:53 -0600

    I don't need my computer to be belligerent towards me. Change the low battery message.

--- scripts/auto-hibernate.sh
+++ scripts/auto-hibernate.sh
@@ -4,7 +4,7 @@ stat=$(cat /sys/class/power_supply/BAT0/uevent | grep STATUS | cut -f2 -d=)
 batperc=$(cat /sys/class/power_supply/BAT0/uevent | grep CAPACITY | cut -f2 -d=)
 if [[ $stat -eq "Discharging" ]]; then
   export DISPLAY=:0.0
-  if [[ $batperc -le 7 ]]; then
+  if [[ $batperc -le 8 ]]; then
     # This will, and should, spam me when my battery gets low.
     notify-send 'Battery is low!' 'Save your work, idiot!' --icon=battery-low
   fi
